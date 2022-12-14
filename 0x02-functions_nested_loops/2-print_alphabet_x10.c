c4fcf07cf763b9a9bfe3696f03a176c5016dbf8b
Adetunji <sincerityisgood@gmail.com>
2022-12-14 07:14:38 -0800

    times_table task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 2-print_alphabet_x10 - entry point
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = ('a');
+	int d = 0;
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	Return (0);
+}
