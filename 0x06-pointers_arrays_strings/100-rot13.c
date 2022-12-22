7611ff5421c845e7ba856b25e7054ec46480ae62
Adetunji <sincerityisgood@gmail.com>
2022-12-22 06:20:00 -0800

    Task 9

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string
+ * @s: the pointer
+ * Return: the coded string
+ */
+
+char *rot13(char *s)
+{
+	char a1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char a2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0;
+	int c;
+
+	while (s[i] != 0)
+	{
+		c = 0;
+		while (a1[c] != 0)
+		{
+			if (s[i] == a1[c])
+			{
+				s[i] = a2[c];
+			break;
+			}
+			c++;
+		}
+		i++;
+	}
+return (s);
+}
