4ec1d8482381c4acc30b169b8abcb3d7eed0bc18
Adetunji <sincerityisgood@gmail.com>
2023-01-04 04:09:02 -0800

    task 2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -11,4 +10,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
-#endif /* _MAIN.H_ */
+#endif /* MAIN_H */
