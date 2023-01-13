7f7ae771632e624d9dd5a1616b449cd160fe1257
Adetunji <sincerityisgood@gmail.com>
2023-01-13 04:05:52 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,13 @@ void more_numbers(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 15; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b > 9)
-			_putchar(b / 10 + '0');
-			_putchar(b % 10 + '0');
+			{
+				_putchar((num / 10) + 48);
+			}
+			_putchar((num % 10) + 48);
 		}
 		_putchar('\n');
 	}
