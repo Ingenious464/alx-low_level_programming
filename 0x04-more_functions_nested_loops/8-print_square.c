7bb069d35d1f4459ce582645fb0570049439dcb9
Adetunji <sincerityisgood@gmail.com>
2023-01-13 05:58:57 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - beginning of program
+ * @size: number of characters the square will be
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
