c70d799740a8356917e44ab3c47e26a34ca801fe
Adetunji <sincerityisgood@gmail.com>
2023-01-31 21:15:20 -0800

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,33 +7,39 @@
  *@n: value of the new node
  *Return: the listint_t list after the new node has been inserted
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *node_to_insert_after;
-	unsigned int i = 0;
+unsigned int a;
+listint_t *b;
+listint_t *c;
 
-	node_to_insert_after = *head;
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	if (idx == 0)
-	{
-		new_node->next = node_to_insert_after;
-		*head = new_node;
-		return (new_node);
-	}
-	while (i < idx - 1)
-	{
-		if (!node_to_insert_after)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		node_to_insert_after = node_to_insert_after->next;
-		i++;
-	}
-	new_node->next = node_to_insert_after->next;
-	node_to_insert_after->next = new_node;
-	return (new_node);
+if (*head == NULL && index != 0)
+return (NULL);
+b = *head;
+c = malloc(sizeof(listint_t));
+if (c == NULL)
+return (NULL);
+c->n = n;
+if (index == 0)
+{
+c->next = *head;
+*head = c;
+return (c);
+}
+b = *head;
+a = 0;
+while (a < index - 1)
+{
+b = b->next;
+if (b == NULL)
+{
+free(c);
+return (NULL);
+}
+a++;
+}
+c->next = b->next;
+b->next = c;
+return (c);
 }
